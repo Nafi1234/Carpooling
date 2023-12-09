49dd52e84c2b72cd6aeca22b7e8d857f659b2541
Nafi 1234 <nafiiqbal789@gmail.com>
2023-12-09 20:06:31 +0530

    ALLOWED Host second

--- carpool/settings.py
+++ carpool/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = 'django-insecure-bqs0e(sh70hdh&#@58i!qo)fj)9fjd5aui$))z1_1ph_ke605%
 AUTH_USER_MODEL='user.User'
 DEBUG = True
 
-ALLOWED_HOSTS = ['54.226.227.141']
-
+ALLOWED_HOSTS = ['54.226.227.141', '127.0.0.1', 'localhost']
 
 # Application definition
 
