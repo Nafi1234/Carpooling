1aa5ed613eab018762024916f528346184cf9f4d
Nafi 1234 <nafiiqbal789@gmail.com>
2023-12-14 20:53:25 +0530

    the url is changed here

--- Chat/routing.py
+++ Chat/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path,path
 from .import consumer
 
 websocket_urlpatterns = [
-    path('ws/chat/<str:room_name>/',consumer.ChatRoomConsumer.as_asgi())
+    path('/chat/<str:room_name>/',consumer.ChatRoomConsumer.as_asgi())
     ]
